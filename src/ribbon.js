78374bd2ca18454eb647cef6e5c76343420ea791
Philippe Rivière <fil@rezo.net>
2020-07-06 12:09:19 +0200

    the inner arc of a ribbon should not cross the outer arc fixes #13
    see https://observablehq.com/d/07db25f490cf93d1

--- src/ribbon.js
+++ src/ribbon.js
@@ -1,6 +1,6 @@
 import {slice} from "./array.js";
 import constant from "./constant.js";
-import {cos, halfPi, sin} from "./math.js";
+import {abs, angularDistance, cos, halfPi, sin} from "./math.js";
 import {path} from "d3-path";
 
 function defaultSource(d) {
@@ -47,13 +47,31 @@ export default function() {
 
     if (!context) context = buffer = path();
 
+    // evaluate angles for the 2 Bezier parts so we know which one is "inner"
+    // D0 D1 are the distances. The largest is "outer"
+    // the inner angle should shrink a little, depending
+    // on whether it can hurt the outer.
+    var D0 = angularDistance(sa0, ta1),
+        D1 = angularDistance(sa1, ta0),
+        inner = Math.pow(abs(D0 - D1) + (1 - D0) * (1 - D1), 6) * tr / 4;
+
     context.moveTo(sx0, sy0);
     context.arc(0, 0, sr, sa0, sa1);
     if (sa0 !== ta0 || sa1 !== ta1) { // TODO sr !== tr?
-      context.quadraticCurveTo(0, 0, tr * cos(ta0), tr * sin(ta0));
+      context.quadraticCurveTo(
+        D0 > D1 && inner * (cos(sa1) + cos(ta0)),
+        D0 > D1 && inner * (sin(sa1) + sin(ta0)),
+        tr * cos(ta0),
+        tr * sin(ta0)
+      );
       context.arc(0, 0, tr, ta0, ta1);
     }
-    context.quadraticCurveTo(0, 0, sx0, sy0);
+    context.quadraticCurveTo(
+      D0 < D1 && inner * (cos(sa0) + cos(ta1)),
+      D0 < D1 && inner * (sin(sa0) + sin(ta1)),
+      sx0,
+      sy0
+    );
     context.closePath();
 
     if (buffer) return context = null, buffer + "" || null;
